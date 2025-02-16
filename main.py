e756e6732a924e143efde813d3e9e76acff6fe41
adixD <adityavikramsinha19@gmail.com>
2025-02-16 15:02:30 +0530

    full automation of a benchmark, starting from making it to geting the data from the final output has been achieved. Only thing left is to start parameterisation & implement HMMs/RFCs/DTs--standard machine learning procedures.

--- main.py
+++ main.py
@@ -2,56 +2,80 @@ import asyncio
 import json
 import os
 
+
 from rush import build_blocking_provider
 from rush.graphql_client import CreateRun
 
-from rush_fetch import benchmark_submission
+from PythonProject.rush_fetch import benchmark_submission
 from rush_mutate import benchmark_mutation
 
 API_TOKEN = os.getenv("BIOHACK_ACCESS_TOKEN")
+
 rex_fn = r"""
 let
-    options = {
-        simulation = {
-            step_size_ps = 0.001,
-            stages = [
-                {
-                    Nvt = {
-                        temperature_kelvin = 300,
-                        steps = 1000
-                    }
-                }
-            ]
-        },
-        trajectory = {
-            interval = 100,
-            info = true
-        }
-    },
-    dojo = \protein_trc -> dojo_rex_s default_runspec options [protein_trc] []
+    auto3d = \smi -> map to_data (get 0 (auto3d_rex_s default_runspec_gpu { k = 1 } [smi])),
+    p2rank = \prot_conf -> p2rank_rex_s default_runspec {} prot_conf,
+    gnina = \prot_conf -> \bounding_box -> \smol_conf ->
+        get 0 (get 0 (gnina_rex_s default_runspec_gpu {} [prot_conf] [bounding_box] smol_conf []))
 in
-    \protein_trc -> dojo protein_trc
+    \input ->
+        let
+            protein = load (id (get 0 input)) "ProteinConformer",
+            smol_id = id (get 1 input),
+            smiles = smi (load smol_id "Smol"),
+            structure = load (structure_id protein) "Structure",
+            trc = [topology structure, residues structure, chains structure],
+            bounding_box = get 0 (get 0 (p2rank trc)),
+            smol_structure = auto3d smiles,
+            docked_structure = gnina trc bounding_box [smol_structure],
+            min_affinity = list_min (map (get "affinity") (get "scores" docked_structure)),
+            binding_affinity = BindingAffinity {
+                affinity = min_affinity,
+                affinity_metric = "kcal/mol",
+                protein_id = protein_id protein,
+                smol_id = smol_id,
+                metadata = Metadata {
+                    name = "Protein Binding Affinity",
+                    description = none,
+                    tags = []
+                }
+            }
+        in
+            [BenchmarkArg { entity = "BindingAffinity", id = save binding_affinity }]
 """
 
 client = build_blocking_provider(access_token=API_TOKEN)
 benchmark = client.benchmark(name="OpenFF Protein-Ligand Binding Benchmark")
 
-# Ideally, we will want to define a bulk mutator and a bulk fetcher,
-# Both of these will do exactly what their name suggests
-# Intermediate controller will be some kind of traffic_controller that can try to
-# ensure that there is almost zero waste in time.
-# The only problem would be to decide what inputs to give bulk mutator, and that needs to be specifically studied under **kwargs
-# since it is relatively un-explored.
+
+
+async def benchmark_workflow(rex, name, sample_pct, with_outs, wait_for_result=True):
+    """
+    Initiates a simple benchmark workflow for the Rush API.
+
+    Parameters:
+        rex (str): The rex function to send
+        name (str): Name of the function
+        sample_pct (float): the % of the sample to run the benchmark on
+        with_outs (bool): withouts from the benchmark_mutation
+        wait_for_result (bool): Whether to fetch the result of the benchmark or not. If not, then only the response containing run_benchmark_id and source_run_id is returned.
+    """
+
+
+    input_ = CreateRun(rex=rex, name=name, project_id = client.project_id)
+    print("✅ Requesting to run Benchmark")
+    response = await benchmark_mutation(input_, benchmark.id, sample_pct=sample_pct, with_outs=with_outs, auth_token= API_TOKEN)
+    print(f"🚀 Running benchmark with submission ID: {response.data['run_benchmark']['id']} \t source submission ID: {response.data['run_benchmark']['source_run']['id']}")
+    if wait_for_result :
+        res = (client.poll_run_blocking(response.data['run_benchmark']['source_run']['id'])).status
+        if res == "DONE":
+            return await benchmark_submission(response.data['run_benchmark']['id'], client.project_id, API_TOKEN)
+    else:
+        return response.data
 
 async def main():
-    input_ = CreateRun(rex=rex_fn,name="Benchmark Request pushed from Adi's Computer", project_id=client.project_id)
-    # response = await benchmark_mutation(input_,benchmark.id,0.02,False,API_TOKEN)
-    # run_benchmark_id = response['run_benchmark']['id']
-
-    # response = await benchmark_submission(run_benchmark_id, client.project_id, auth_token=API_TOKEN)
-    # print(json.dumps(response.data, indent=1))  # Print the API response
-
+    res = await benchmark_workflow(rex=rex_fn, name="Benchmark Workflow Submission from Adi's Computer", sample_pct=0.01, with_outs=False)
+    print(json.dumps(res, indent=1))
 
 if __name__ == "__main__":
     asyncio.run(main())
-
--- rush_mutate.py
+++ rush_mutate.py
@@ -22,57 +22,8 @@ MUTATION_QUERY="""
 # GraphQL Mutation
 MUTATION_RUN_BENCHMARK = gql(MUTATION_QUERY)
 
-rex_code = r"""
-let
-    auto3d = \smi -> map to_data (get 0 (auto3d_rex_s default_runspec_gpu { k = 1 } [smi])),
-
-    p2rank = \prot_conf -> p2rank_rex_s default_runspec {} prot_conf,
-
-    gnina = \prot_conf -> \bounding_box -> \smol_conf ->
-        get 0 (get 0 (gnina_rex_s default_runspec_gpu {} [prot_conf] [bounding_box] smol_conf [])),
-
-in
-\input ->
-    let
-        protein = load (id (get 0 input)) "ProteinConformer",
-        smol_id = id (get 1 input),
-        smiles = smi (load smol_id "Smol"),
-
-        structure = load (structure_id protein) "Structure",
-        trc = [
-            topology structure,
-            residues structure,
-            chains structure
-        ],
-
-        bounding_box = get 0 (get 0 (p2rank trc)),
-
-        smol_structure = auto3d smiles,
-
-        docked_structure = gnina trc bounding_box [smol_structure],
-
-        min_affinity = list_min (map (get "affinity") (get "scores" docked_structure)),
-
-        binding_affinity = BindingAffinity {
-            affinity = min_affinity,
-            affinity_metric = "kcal/mol",
-            protein_id = protein_id protein,
-            smol_id = smol_id,
-            metadata = Metadata {
-                name = "binding affinity for smol and protein",
-                description = none,
-                tags = []
-            }
-        }
-    in
-        [BenchmarkArg {
-            entity = "BindingAffinity",
-            id = save binding_affinity
-        }]
-"""
 # Function to execute GraphQL mutation
 async def benchmark_mutation(input_data, benchmark_id, sample_pct, with_outs, auth_token):
-    print(f"🚀 Running benchmark with ID: {benchmark_id}")
 
     transport = AIOHTTPTransport(
         url=GRAPHQL_ENDPOINT,
@@ -89,11 +40,9 @@ async def benchmark_mutation(input_data, benchmark_id, sample_pct, with_outs, au
                 "sample_pct": sample_pct,
                 "with_outs": with_outs
             }
-# 59dfc6a0-c77d-4373-9a39-b550110e89de
+
             return await session.execute(MUTATION_RUN_BENCHMARK, variable_values=variables, operation_name="run_benchmark", serialize_variables=True, get_execution_result=True)
 
         except Exception as e:
             print(f"❌ Error: {e}")
             return None
-
-
